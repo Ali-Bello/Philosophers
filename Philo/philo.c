3524c71e3dcab1044e7fe2e10d575e8b44996825
Ali Derraj <aderraj@c4r2p9.1337.ma>
2024-08-28 03:50:32 +0100

    mutex init

--- Philo/philo.c
+++ Philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: aderraj <aderraj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 04:53:07 by aderraj           #+#    #+#             */
-/*   Updated: 2024/08/25 18:25:29 by aderraj          ###   ########.fr       */
+/*   Updated: 2024/08/28 03:50:19 by aderraj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,8 @@ void    ft_clean(t_info *info)
         free(info->philos);
     if (info->forks)
         free(info->forks);
-    if (info->philos_id)
-        free(info->philos_id);
+    memset(info, 0, sizeof(t_info));
 }
-
 long    get_timestamp()
 {
     struct timeval time;
@@ -29,6 +27,7 @@ long    get_timestamp()
     gettimeofday(&time, NULL);
     return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
 }
+
 int init_objects(t_info *info)
 {
     int i;
@@ -43,15 +42,12 @@ int init_objects(t_info *info)
     while (i < info->num_of_philos)
     {
         memset(&info->philos[i], 0, sizeof(t_philo));
-        info->philos[i].start_time = info->timestamp;
+        info->philos[i].info = info;
         info->philos[i].philo_no = i + 1;
         if (pthread_mutex_init(&info->forks[i], NULL))
             return (ft_clean(info), 1);
         i++;
     }
-    info->philos_id = malloc(sizeof(pthread_t) * info->num_of_philos);
-    if (!info->philos_id)
-        return (ft_clean(info), 1);
     if (pthread_mutex_init(&info->time_mutex, NULL))
         return (ft_clean(info), 1);
     return (0);
@@ -62,7 +58,18 @@ void    *activity(void *arg)
     t_philo *philo;
 
     philo = (t_philo *)arg;
-    printf("%ld philo no = %d\n", get_timestamp() - philo->start_time, philo->philo_no);
+    pthread_mutex_lock(&philo->info->time_mutex);
+    while (!philo->info->death_flag)
+    {
+        long time = get_timestamp() - philo->info->start_time;
+        printf("%ldms philo %d is thinking %d\n", time, philo->philo_no, philo->status);
+        if (time > philo->info->time_to_eat)
+        {
+            philo->info->death_flag = 1;
+            printf("%ldms philo %d is dead\n", time, philo->philo_no);
+        }   
+    }
+    pthread_mutex_unlock(&philo->info->time_mutex);
     return (NULL);
 }
 
@@ -74,19 +81,20 @@ int main(int ac, char **av)
     memset(&info, 0, sizeof(t_info));
     if (check_input(ac, av, &info))
         return (0);
-    info.timestamp = get_timestamp();
     if (init_objects(&info))
         return (0);
+    info.start_time = get_timestamp();
     i = 0;
     while (i < info.num_of_philos)
     {
-        if (pthread_create(&info.philos_id[i], NULL, activity, &info.philos[i]))
+        if (pthread_create(&info.philos[i].ptid, NULL,\
+            activity, &info.philos[i]))
             return (ft_clean(&info), 0);
         i++;
     }
     i = 0;
     while (i < info.num_of_philos)
-        pthread_join(info.philos_id[i++], NULL);
+        pthread_join(info.philos[i++].ptid, NULL);
     ft_clean(&info);
     return (0);
 }
